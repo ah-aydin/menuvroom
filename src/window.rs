8fcafefcffab149aa23623dbdbd3e588fc7679db
ah-aydin <ahamzaaydin.py@gmail.com>
2024-09-22 20:07:54 +0200

    Hide menu bar

--- src/window.rs
+++ src/window.rs
@@ -1,5 +1,6 @@
 #![allow(non_snake_case)]
 
+use dioxus::desktop::{Config, WindowBuilder};
 use dioxus::prelude::*;
 use dioxus_desktop::tao::dpi::LogicalPosition;
 use dioxus_desktop::tao::dpi::LogicalSize;
@@ -31,23 +32,28 @@ fn App() -> Element {
 }
 
 pub fn window_main() {
-    let builder = dioxus_desktop::WindowBuilder::new()
-        .with_title("menuvroom")
-        // TODO compute size according to display resolution
-        .with_inner_size(LogicalSize::new(800, 600))
-        // TODO compute position according to the window size and display resolution
-        .with_position(LogicalPosition::new(300, 200))
-        .with_focused(true)
-        .with_decorations(false)
-        .with_transparent(false)
-        .with_always_on_top(true)
-        .with_fullscreen(None)
-        .with_resizable(false)
-        .with_closable(true)
-        .with_minimizable(false)
-        .with_maximized(false)
-        .with_theme(None);
-
-    let config = dioxus_desktop::Config::new().with_window(builder);
-    dioxus_desktop::launch::launch(App, vec![], config);
+    LaunchBuilder::desktop()
+        .with_cfg(
+            Config::new()
+                .with_window(
+                    WindowBuilder::new()
+                        .with_title("menuvroom")
+                        // TODO compute size according to display resolution
+                        .with_inner_size(LogicalSize::new(800, 600))
+                        // TODO compute position according to the window size and display resolution
+                        .with_position(LogicalPosition::new(300, 200))
+                        .with_focused(true)
+                        .with_decorations(false)
+                        .with_transparent(false)
+                        .with_always_on_top(true)
+                        .with_fullscreen(None)
+                        .with_resizable(false)
+                        .with_closable(true)
+                        .with_minimizable(false)
+                        .with_maximized(false)
+                        .with_theme(None),
+                )
+                .with_menu(None),
+        )
+        .launch(App);
 }
